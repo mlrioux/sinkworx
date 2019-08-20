2801ee185603793ebfb7c7db546b3497f1ab2bfb
Melissa Rioux <mrioux@M4163.local>
2019-08-20 14:18:00 -0300

    Changed some function names.

--- R/import functions.R
+++ R/import functions.R
@@ -2,7 +2,7 @@
 
 #'@title Import cell sinking data from BMG Labtech plate spectrophotometers
 #'@description Transforms sinking data from plate format into tidy format. Works on .txt and .csv output from FLUOStar Omega and CLARIOStar plate readers.
-#'@usage sink_bmg(data_folder, catalog = NA, time_units = "sec", RFU_cutoff = 10, skip = 1,
+#'@usage import_bmg(data_folder, catalog = NA, time_units = "sec", RFU_cutoff = 10, skip = 1,
 #'export_output = FALSE, save_as = "Sink_Database.csv")
 #'@param data_folder folder containing the data files to be read, in quotations. Should be in the working directory.
 #'@param catalog optional dataframe containing experimental metadata that can be merged with the imported data files. Should include a "Plate" column containing the filename (including file extension if .csv), and
@@ -14,10 +14,10 @@
 #'@param save_as desired filename, if results are to be exported.
 #'@return A single dataframe containing tidied data from all files in data_folder, as well as experimental metadata if catalog is supplied. Creates the columns "Well", "Elapsed.Time.m", "RFU", and "Plate" (filename without extension).
 #'@examples
-#'sink_bmg("Sinking BMG", time_units = "min", RFU_cutoff = 7)
+#'import_bmg("Sinking BMG", time_units = "min", RFU_cutoff = 7)
 # --------------------------------------------------------------------------------
 
-sink_bmg <- function(data_folder, catalog = NA, time_units = "sec", RFU_cutoff = 10, skip = 1,
+import_bmg <- function(data_folder, catalog = NA, time_units = "sec", RFU_cutoff = 10, skip = 1,
                      export_output = FALSE, save_as = "Sink_Database.csv") {
 
 # ---------------------------------------------------------------------------------
@@ -113,7 +113,7 @@ sink_bmg <- function(data_folder, catalog = NA, time_units = "sec", RFU_cutoff =
 
 #'@title Import cell sinking data from Molecular Devices Spectramax EM plate spectrophotometer
 #'@description Transforms sinking data from plate format into tidy format. Works on .txt output from Spectramax EM plate reader.
-#'@usage sink_spmx(data_folder, catalog = NA, time_units = "sec", RFU_cutoff = 2, skip = 3,
+#'@usage import_spmx(data_folder, catalog = NA, time_units = "sec", RFU_cutoff = 2, skip = 3,
 #'export_output = FALSE, save_as = "Sink_Database.csv" )
 #'@param data_folder folder containing the data files to be read, in quotations. Should be in the working directory.
 #'@param catalog optional dataframe containing experimental metadata that can be merged with the imported data files. Should include a "Plate" column containing the filename, and
@@ -125,7 +125,7 @@ sink_bmg <- function(data_folder, catalog = NA, time_units = "sec", RFU_cutoff =
 #'@param save_as desired filename, if results are to be exported.
 #'@return A single dataframe containing tidied data from all files in data_folder, as well as experimental metadata if catalog is supplied. Creates the columns "Well", "Elapsed.Time.m", "RFU", and "Plate" (filename without extension).
 #'@examples
-#'sink_spmx("Spectramax data", culture_catalog, time_units = "min", RFU_cutoff = 7, save_as = "Sinking.csv")
+#'import_spmx("Spectramax data", culture_catalog, time_units = "min", RFU_cutoff = 7, save_as = "Sinking.csv")
 #'
 #'sinking_data <- sink_spmx("Spectramax data", export_output = FALSE)
 #'
@@ -133,7 +133,7 @@ sink_bmg <- function(data_folder, catalog = NA, time_units = "sec", RFU_cutoff =
 
 # -----------------------------------------------------------------------------------
 
-sink_spmx <- function(data_folder, catalog = NA, time_units = "sec", RFU_cutoff = 2, skip = 3,
+import_spmx <- function(data_folder, catalog = NA, time_units = "sec", RFU_cutoff = 2, skip = 3,
                       export_output = FALSE, save_as = "Sink_Database.csv") {
 
 # -----------------------------------------------------------------------------
